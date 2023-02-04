7f043d457eb31f2b2b8dd2fba0c13b6047a30f7f
mibachcha1 <68137662+mibachcha1@users.noreply.github.com>
2023-02-03 20:48:48 -0600

    Made index.html root + other changes

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+#from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+from django.shortcuts import render
+def index(request):
+
+    return render(request, "index.html")
