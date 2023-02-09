fa8d0794c2e8605845f7aa7cd5e6d60a1b322704
mibachcha1 <68137662+mibachcha1@users.noreply.github.com>
2023-02-09 12:47:07 -0600

    Updated Models
    Added Ratings Model; Updated Restaurant Model to Allow for One owner to have multiple restaurants

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from restaurants.models import Restaurant, Owner, Menu, FoodItem # Reviews,Ratings
+from restaurants.models import Restaurant, Owner, Menu, FoodItem, Reviews #,Ratings
 
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(Owner)
 admin.site.register(Menu)
 admin.site.register(FoodItem)
+admin.site.register(Reviews)
 
-# admin.site.register(Reviews)
 # admin.site.register(Ratings)
