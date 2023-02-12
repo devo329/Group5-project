102af0ea6cd4486ca1e5bf5956410c6fc028df51
mibachcha1 <68137662+mibachcha1@users.noreply.github.com>
2023-02-11 21:28:47 -0600

    New Type Model

--- restaurants/models.py
+++ restaurants/models.py
@@ -4,6 +4,12 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Type(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Owner(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
