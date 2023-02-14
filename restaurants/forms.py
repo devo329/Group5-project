d02b839ee32e76a4db0da928de50f71f9733d40d
devo329 <dedwar29@depaul.edu>
2023-02-14 12:18:20 -0600

    Login/Logout and Registration

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -1,7 +1,26 @@
 from django import forms
 from .models import FoodItem, Reviews
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Reviews
         fields = ['rating', 'review']
+        
+        
+
+class NewUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,9 +3,14 @@ from . import views
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path("register", views.register_request, name="register"),
     path("", views.index, name="index"),
     path("loading/restaurant/", views.loading_screen,),
     path("restaurant/", views.restaurant,name='restaurant'),
     path('review/submit/', views.addReview, name='review_submit'),
     path('like/<int:item_id>/<restaurant_id>', views.like, name='like'),
+    path("logout", views.logout_request, name= "logout"),
+
+    
+
 ]
